cc14bd60713110625b09a57b95a1015489744153
Harryson Barbosa Palheta <harryson.palheta@itbam.org.br>
2020-03-10 10:18:28 -0400

    force env

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -11,11 +11,11 @@ export const environment = {
   //backend: 'http://172.100.10.112:8081/login', // Put your backend here
   backend: '', 
   origin: {
-    transports: 'http://localhost:3001/auth/v1',
-    tracker:    'http://localhost:3001/auth/v1',
-    users:      'http://localhost:3001/auth/v1',
-    employees:  'http://localhost:3001/auth/v1',
-    // transports: 'http://52.229.33.51:8080/auth/v1',
+    transports: 'http://rcr-api.herokuapp.com:3001/auth/v1',
+    tracker:    'http://rcr-api.herokuapp.com:3001/auth/v1',
+    users:      'http://rcr-api.herokuapp.com:3001/auth/v1',
+    employees:  'http://rcr-api.herokuapp.com:3001/auth/v1',
+   // transports: 'http://52.229.33.51:8080/auth/v1',
     // tracker: 'http://52.229.33.51:8080/auth/v1',
     // users: 'http://52.229.33.51:8080/auth/v1',
     // employees: 'http://52.229.33.51:8080/auth/v1',
