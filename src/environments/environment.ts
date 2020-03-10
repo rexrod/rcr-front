65281848214458aece54ddbb24b5a0e85f8d2b12
Harryson Barbosa Palheta <harryson.palheta@itbam.org.br>
2020-03-10 08:36:39 -0400

    fix envs to heroku

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,23 +1,13 @@
-// The file contents for the current environment will overwrite these during build.
-// The build system defaults to the dev environment which uses `environment.ts`, but if you do
-// `ng build --env=prod` then `environment.prod.ts` will be used instead.
-// The list of which env maps to which file can be found in `angular-cli.json`.
-
 
 export const environment = {
   production: false,
   googleMapsApiKey: '', // key from w3school
-  // backend: 'http://localhost:4200/login', // Put your backend here
   backend: '', 
   origin: {
-    // transports: 'http://52.229.33.51:8080/auth/v1',
-    // tracker:    'http://52.229.33.51:8080/auth/v1',
-    // users:      'http://52.229.33.51:8080/auth/v1',
-    // employees:  'http://52.229.33.51:8080/auth/v1',
-    transports: 'http://localhost:3001/auth/v1',
-    tracker:    'http://localhost:3001/auth/v1',
-    users:      'http://localhost:3001/auth/v1',
-    employees:  'http://localhost:3001/auth/v1',
+    transports: 'http://rcr-api.herokuapp.com:3001/auth/v1',
+    tracker:    'http://rcr-api.herokuapp.com:3001/auth/v1',
+    users:      'http://rcr-api.herokuapp.com:3001/auth/v1',
+    employees:  'http://rcr-api.herokuapp.com:3001/auth/v1',
 
 // from template    
     notices: '',
